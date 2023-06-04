b96a958079b79a781fca772f9963c81bbabd8d0a
Gustavo Cooper <gustavoxcooper@gmail.com>
2023-06-04 09:52:56 -0300

    Update Utils.h

--- Utils.h
+++ Utils.h
@@ -11,6 +11,9 @@ public:
 	bool containsSpace(string& string);
 	bool isEmailValid(string& email);
 	bool containsOnlyNumbers(string& string);
+	bool shorterThan(string& string, int maxSize);
+
+	// bool foundSameString(string& stringBeingTested, string& existingString); Don't know if it'll be needed
 };
 
 
