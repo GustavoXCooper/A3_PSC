e7269fb08d5761485b3cadf2c09004d605fa7e87
Gustavo Cooper <gustavoxcooper@gmail.com>
2023-06-04 09:56:25 -0300

    Update Trabalho Final.cpp

--- Trabalho Final.cpp
+++ Trabalho Final.cpp
@@ -1,6 +1,7 @@
 
 #include <iostream>
 #include "Date.h";
+#include "Book.h";
 
 int main()
 {
@@ -17,4 +18,10 @@ int main()
     date.SetMonth(11);
     date.SetYear(1501);
     cout << date.GetFullDate() << endl;
+    
+    Book* bookTeste = new Book("title", 100, "só testando o livro", "inteiro", "genreTest", true, 10.03, "Eu Mesmo", "Euzinho");
+
+    // Dúvida! As restrições foram todas feitas nos métodos set, seria melhor preencher um por um dos valores? Ou criar restrições no código novamente?
+    bookTeste->addNewGenre("Teste Gênero");
+    bookTeste->showBookInfo();
 }
