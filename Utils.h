c762794fb597f688eeaa61abc998c84cc1704fa4
Gustavo Cooper <gustavoxcooper@gmail.com>
2023-06-05 12:00:38 -0300

    Update Utils.h

--- Utils.h
+++ Utils.h
@@ -3,6 +3,7 @@
 #include <iostream>
 using namespace std;
 
+
 class Utils {
 public:
 	Utils();
@@ -11,10 +12,17 @@ public:
 	bool containsSpace(string& string);
 	bool isEmailValid(string& email);
 	bool containsOnlyNumbers(string& string);
-	bool shorterThan(string& string, int maxSize);
+	bool shorterThan(string& string);
 
 	// bool foundSameString(string& stringBeingTested, string& existingString); Don't know if it'll be needed
-};
 
+	// more specific utils
+	string setAuthorName();
+	string setBookTitle();
+	string chooseFromAllAvailableGenres();
+	string chooseFromAllConditions();
+	bool isBargainable();
+	string setPublishingCompany();
+};
 
 #endif
