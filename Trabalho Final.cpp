49f96f3a806a1d6bb69cef7ca95f24e6bcaa13a9
Gustavo Cooper <gustavoxcooper@gmail.com>
2023-06-10 12:00:13 -0300

    Update Trabalho Final.cpp

--- Trabalho Final.cpp
+++ Trabalho Final.cpp
@@ -1,27 +1,91 @@
-
 #include <iostream>
-#include "Date.h";
-#include "Book.h";
+#include "Seller.h"
+#include "Book.h"
+#include <iomanip>
 
 int main()
 {
-    Date date(1, 9, 2001);
-    cout << date.GetDay() << endl;
-    cout << date.GetMonth() << endl;
-    cout << date.GetYear() << endl;
-    cout << date.GetFullDate() << endl;
-    date.SetDay(35);
-    date.SetMonth(0);
-    date.SetYear(1499);
-    cout << date.GetFullDate() << endl;
-    date.SetDay(1);
-    date.SetMonth(11);
-    date.SetYear(1501);
-    cout << date.GetFullDate() << endl;
-    
-    Book* bookTeste = new Book("title", 100, "só testando o livro", "inteiro", "genreTest", true, 10.03, "Eu Mesmo", "Euzinho");
+	Date* dateTeste = new Date(10, 9, 2001);
+	User* teste = new User("gustavo", "cooper", *dateTeste, "51993855818", "gusxavie8@gmail.com", "123");
+	Book bookTeste("Teste", 10, "texto texto", "inteiro", "fantasia", true, 10.2, "teste Autor", "qualquer Uma");
+	Seller* sellerTeste = new Seller("gustavo", "cooper", *dateTeste, "51993855818", "gusxavie8@gmail.com", "123");
 
-   // Dúvida! As restrições foram todas feitas nos métodos set, mas o construtot não recebe essas restrições. O que eu faço? Criar restrições novamente quando for chamar o construtor??
-    bookTeste->addNewGenre("Teste Gênero");
-    bookTeste->showBookInfo();
+	cout << sellerTeste->getName() << endl;
+	cout << sellerTeste->getSurname() << endl;
+	cout << sellerTeste->getNameCompelte() << endl;
+	cout << sellerTeste->getPassword() << endl;
+	cout << sellerTeste->getEmail() << endl;
+	cout << sellerTeste->getPhoneNumber() << endl;
+
+
+	sellerTeste->addNewBook();
+	//sellerTeste->addNewBook();
+	sellerTeste->editBook();
+	//sellerTeste->removeBook();
+
+
+	for (int i = 0; i < 1; i++) {
+		cout << "\nTitle: " << sellerTeste->getBook(i).getTitle();
+		cout << "\nPages: " << sellerTeste->getBook(i).getPages();
+		cout << "\nCondição: " << sellerTeste->getBook(i).getCondition();
+		cout << "\nGênero: " << sellerTeste->getBook(i).getGenres(0);
+		cout << "\nNegociável? " << sellerTeste->getBook(i).getBargainable();
+		cout << "\nPreço: " << fixed << setprecision(sellerTeste->getBook(i).getPrice());
+		cout << "\nAutor: " << sellerTeste->getBook(i).getAuthor();
+		cout << "\nEditora: " << sellerTeste->getBook(i).getPublishingCompany();
+	}
+
+	/*
+	cout << teste->getName() << endl;
+	cout << teste->getSurname() << endl;
+	cout << teste->getNameCompelte() << endl;
+	cout << teste->getPassword() << endl;
+	cout << teste->getEmail() << endl;
+	cout << teste->getPhoneNumber() << endl;
+
+
+	teste->setName("test @");
+	teste->setSurname("tes@te");
+	teste->setPassword("1234");
+	teste->setEmail("teste");
+	teste->setPhoneNumber("12a31");
+
+	cout << teste->getName() << endl;
+	cout << teste->getSurname() << endl;
+	cout << teste->getNameCompelte() << endl;
+	cout << teste->getPassword() << endl;
+	cout << teste->getEmail() << endl;
+	cout << teste->getPhoneNumber() << endl;
+	*/
+	/*
+	cout << bookTeste.getTitle() << endl;
+	cout << bookTeste.getBookSummary() << endl;
+	cout << bookTeste.getBargainable() << endl;
+	cout << bookTeste.getCondition()<< endl;
+	cout << bookTeste.getPages() << endl;
+	cout << bookTeste.getPrice() << endl;
+	cout << bookTeste.getPublishingCompany() << endl;
+	cout << bookTeste.getAuthor() << endl;
+	//cout << bookTeste.getGenres[0]<< endl;
+
+	bookTeste.setTitle();
+	bookTeste.setPublishingCompany();
+	bookTeste.setCondition();
+	bookTeste.setBargainable();
+	bookTeste.setPages(1000);
+	bookTeste.setPrice();
+
+
+	cout << bookTeste.getTitle() << endl;
+	cout << bookTeste.getBookSummary() << endl;
+	cout << bookTeste.getBargainable() << endl;
+	cout << bookTeste.getCondition() << endl;
+	cout << bookTeste.getPages() << endl;
+	cout << bookTeste.getPrice() << endl;
+	cout << bookTeste.getPublishingCompany() << endl;
+	cout << bookTeste.getAuthor() << endl;
+	*/
+
+
+
 }
